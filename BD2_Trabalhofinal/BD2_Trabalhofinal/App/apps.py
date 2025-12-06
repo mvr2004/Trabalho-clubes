8070cb393987a7ae9eb675c081659fc13aedf60a
Vicente <marcovicente200411@gmail.com>
2025-12-06 16:25:16 +0000

    inserir dados no runserver caso nao existem

--- BD2_Trabalhofinal/BD2_Trabalhofinal/App/apps.py
+++ BD2_Trabalhofinal/BD2_Trabalhofinal/App/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
-
-
+from django.db.utils import OperationalError
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -9,23 +8,14 @@ class AppConfig(AppConfig):
     def ready(self):
         import BD2_Trabalhofinal.App.signals  # Importa os sinais
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        # Inserir dados iniciais se não houver
+        try:
+            from .models import P_Associacao  # importa um dos teus models
+            if not P_Associacao.objects.exists():
+                # importa e executa o teu comando inserir_dados
+                from .management.commands.inserir_dados import Command
+                cmd = Command()
+                cmd.handle()
+        except OperationalError:
+            # A base ainda não existe ou as migrações não foram aplicadas
+            pass
